3712a58556094e4ddcda88e360aad394c6b95fa3
baiyunhui123 <baiyunhui123@gmail.com>
2026-09-02 19:11:05 +0000

    Deploying to gh-pages from @ baiyunhui123/baiyunhui.github.io@5d9d4dda85b65fd61fe9bf763e90366d920ee352 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "baiyunhui123/baiyunhui123.github.io",
+    "data-repo": "baiyunhui123/baiyunhui.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
